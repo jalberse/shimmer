33d9be960c1df8769a860187dca988a99f4b7e7f
John Alberse <alberse.john@gmail.com>
2023-09-03 21:27:07 -0500

    Remove unnecessary bounds on Length trait

--- src/vecmath/length.rs
+++ src/vecmath/length.rs
@@ -1,13 +1,4 @@
-use std::ops::{Add, Mul};
-
-use crate::math::Sqrt;
-
-use super::has_nan::HasNan;
-
-pub trait Length<T>: HasNan
-where
-    T: Mul<Output = T> + Add<Output = T> + Sqrt,
-{
+pub trait Length<T> {
     fn length_squared(&self) -> T;
     fn length(&self) -> T;
 }
