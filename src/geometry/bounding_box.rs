a25923ee2544747f33ed7470e3aeba49b76cacff
John Alberse <alberse.john@gmail.com>
2023-09-04 19:03:48 -0500

    Type aliases for Bounds

--- src/geometry/bounding_box.rs
+++ src/geometry/bounding_box.rs
@@ -1,9 +1,17 @@
 use std::marker::PhantomData;
 
+use crate::Float;
+
 use super::vecmath::{
     point::{Point2, Point3},
     tuple::TupleElement,
+    Point2f, Point2i, Point3f, Point3i,
 };
+
+pub type Bounds2i = Bounds2<Point2i, i32>;
+pub type Bounds2f = Bounds2<Point2f, Float>;
+pub type Bounds3i = Bounds2<Point3i, i32>;
+pub type Bounds3f = Bounds2<Point3f, Float>;
 
 struct Bounds2<P, T>
 where
