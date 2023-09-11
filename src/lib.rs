920c495c2836dcf28efd17207b256f414d2129a5
John Alberse <alberse.john@gmail.com>
2023-09-11 13:46:32 -0500

    Add square matrix
    Currently just initialization with a default for identity matrix.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod geometry;
 mod is_nan;
 mod math;
 mod sampling;
+mod square_matrix;
 
 // For convenience, re-export.
 pub use float::Float;
---
+++ src/square_matrix.rs
@@ -0,0 +1,23 @@
+use crate::float::Float;
+
+pub struct SquareMatrix<const N: usize> {
+    pub m: [[Float; N]; N],
+}
+
+impl<const N: usize> SquareMatrix<N> {
+    pub fn new(m: [[Float; N]; N]) -> Self {
+        Self { m }
+    }
+}
+
+impl<const N: usize> Default for SquareMatrix<N> {
+    fn default() -> Self {
+        let mut m: [[Float; N]; N] = [[0.0; N]; N];
+        for i in 0..N {
+            for j in 0..N {
+                m[i][j] = if i == j { 1.0 } else { 0.0 };
+            }
+        }
+        Self::new(m)
+    }
+}
