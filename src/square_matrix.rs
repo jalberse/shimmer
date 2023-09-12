49afab8521b39bbdfd99c1a75c9ec35b95ed2855
John Alberse <alberse.john@gmail.com>
2023-09-12 10:10:24 -0500

    Add SquareMatrix::diag()

--- src/square_matrix.rs
+++ src/square_matrix.rs
@@ -19,6 +19,18 @@ impl<const N: usize> SquareMatrix<N> {
         Self::new(m)
     }
 
+    /// Sets the diagonal of the matrix to the provided values with other values 0.
+    pub fn diag(vals: [Float; N]) -> Self {
+        // PAPERDOC - Rust const generics allow this nice method of passing values in,
+        // where PBRTv4 uses variable length parameter lists.
+        let mut m: [[Float; N]; N] = [[0.0; N]; N];
+        for i in 0..N {
+            m[i][i] = vals[i];
+        }
+        Self::new(m)
+    }
+
+    /// Gets N for an N x N matrix.
     pub fn dim(&self) -> usize {
         N
     }
@@ -26,7 +38,6 @@ impl<const N: usize> SquareMatrix<N> {
 
 impl<const N: usize> Default for SquareMatrix<N> {
     fn default() -> Self {
-        // TODO check that this is correct for identity - does identity include homogeneous coordinate?
         let mut m: [[Float; N]; N] = [[0.0; N]; N];
         for i in 0..N {
             for j in 0..N {
@@ -38,11 +49,31 @@ impl<const N: usize> Default for SquareMatrix<N> {
 }
 
 mod tests {
+    use crate::Float;
+
     use super::SquareMatrix;
 
     #[test]
     fn zero() {
         let m = SquareMatrix::<4>::zero();
-        // TODO use m.dim()
+        for i in 0..m.dim() {
+            for j in 0..m.dim() {
+                assert_eq!(0.0, m.m[i][j]);
+            }
+        }
+    }
+
+    #[test]
+    fn diag() {
+        let m = SquareMatrix::<4>::diag([0.0, 1.0, 2.0, 3.0]);
+        for i in 0..m.dim() {
+            for j in 0..m.dim() {
+                if i == j {
+                    assert_eq!(i as Float, m.m[i][j]);
+                } else {
+                    assert_eq!(0.0, m.m[i][j])
+                }
+            }
+        }
     }
 }
