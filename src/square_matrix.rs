792373b1193aca482c420158dc327ee751f47311
John Alberse <alberse.john@gmail.com>
2023-09-12 09:50:46 -0500

    transfering workstations

--- src/square_matrix.rs
+++ src/square_matrix.rs
@@ -8,10 +8,25 @@ impl<const N: usize> SquareMatrix<N> {
     pub fn new(m: [[Float; N]; N]) -> Self {
         Self { m }
     }
+
+    pub fn zero() -> Self {
+        let mut m: [[Float; N]; N] = [[0.0; N]; N];
+        for i in 0..N {
+            for j in 0..N {
+                m[i][j] = 0.0;
+            }
+        }
+        Self::new(m)
+    }
+
+    pub fn dim(&self) -> usize {
+        N
+    }
 }
 
 impl<const N: usize> Default for SquareMatrix<N> {
     fn default() -> Self {
+        // TODO check that this is correct for identity - does identity include homogeneous coordinate?
         let mut m: [[Float; N]; N] = [[0.0; N]; N];
         for i in 0..N {
             for j in 0..N {
@@ -19,5 +34,15 @@ impl<const N: usize> Default for SquareMatrix<N> {
             }
         }
         Self::new(m)
+    }
+}
+
+mod tests {
+    use super::SquareMatrix;
+
+    #[test]
+    fn zero() {
+        let m = SquareMatrix::<4>::zero();
+        // TODO use m.dim()
     }
 }
