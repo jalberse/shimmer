8ceccb65b92e6124df41b9ad25d44f3fc04c5b6b
John Alberse <alberse.john@gmail.com>
2023-09-20 12:45:23 -0500

    Use SpectrumI and NewTypes for variant enums
    Use a combination SpectrumI trait and Spectrum enum to organize
    the various spectrum types. This lets us implement functions for
    only one function variant e.g. and use variants as solo types
    if desired, while still being able to use the Enum for
    runtime polymorphism when requested.
    
    If this is too wordy for some, we could have used enum_dispatch
    to generate the Spectrum enum automatically. But for clarity,
    we'll go with this approach for now.

--- src/spectrum.rs
+++ src/spectrum.rs
@@ -7,129 +7,45 @@ const LAMBDA_MAX: Float = 830.0;
 
 // TODO Embedded spectral data 4.5.3.
 
-/// The emitted radiance for blackbody at wavelength lambda (nanometers) at temperature (kelvin).
-pub fn blackbody(lambda: Float, temperature: Float) -> Float {
-    if temperature < 0.0 {
-        return 0.0;
-    }
-    let c: Float = 299792458.0;
-    let h: Float = 6.62606957e-34;
-    let kb: Float = 1.3806488e-23;
-    // Convert to meters
-    let l = lambda * 1e-9;
-    // TODO consider Exponentiation by squaring for powi call, and fastexp for exp().
-    let le = (2.0 * h * c * c) / l.powi(5) * (Float::exp((h * c) / (l * kb * temperature)) - 1.0);
-    debug_assert!(!le.is_nan());
-    le
-}
-
-trait SpectrumI {
+pub trait SpectrumI {
     fn get(&self, lambda: Float) -> Float;
 
     fn max_value(&self) -> Float;
 }
 
-enum Spectrum {
+pub enum Spectrum {
     Constant(Constant),
-    DenselySampled {
-        lambda_min: i32,
-        lambda_max: i32,
-        values: Vec<Float>,
-    },
-    /// Normalized blackbody spectrum where the maximum value at any wavelength is 1.
-    Blackbody {
-        /// Temperature K
-        t: Float,
-        normalization_factor: Float,
-    },
+    DenselySampled(DenselySampled),
+    Blackbody(Blackbody),
 }
 
-impl Spectrum {
-    /// Samples from the provided spectrum to create a DenselySampled spectrum
-    pub fn densley_sampled(spectrum: &Spectrum, lambda_min: i32, lambda_max: i32) -> Spectrum {
-        // PAPERDOC This is a fun area where idiomatic rust code (map -> collect) is arguably cleaner
-        // than similar C++ code (allowing e.g. const correctness).
-        let values: Vec<Float> = (lambda_min..lambda_max)
-            .map(|lambda: i32| spectrum.get(lambda as Float))
-            .collect();
-        Spectrum::DenselySampled {
-            lambda_min,
-            lambda_max,
-            values,
-        }
-    }
-
-    pub fn blackbody(t: Float) -> Spectrum {
-        let lambda_max = 2.8977721e-3 / t; // Wien's displacement law
-        let normalization_factor = 1.0 / blackbody(lambda_max * 1e9, t);
-        Spectrum::Blackbody {
-            t,
-            normalization_factor,
-        }
-    }
-
+impl SpectrumI for Spectrum {
     /// Gets the value of the spectral distribution at wavelength lambda
-    pub fn get(&self, lambda: Float) -> Float {
+    fn get(&self, lambda: Float) -> Float {
         // PAPERDOC - this will be a great example against PBRTv4's call with TaggedPointer
-        //  to achieve static dispatch.
+        // to achieve static dispatch. If this is too verbose for someone's taste, then
+        // there are crates like enum_dispatch which could generate this automatically from
+        // the SpectrumI trait.
         match self {
             Spectrum::Constant(c) => c.get(lambda),
-            Spectrum::DenselySampled {
-                lambda_min,
-                lambda_max: _,
-                values,
-            } => {
-                let offset = lambda as i32 - lambda_min;
-                if offset < 0 || offset > values.len() as i32 {
-                    return 0.0;
-                }
-                values[offset as usize]
-            }
-            Spectrum::Blackbody {
-                t,
-                normalization_factor,
-            } => blackbody(lambda, *t) * normalization_factor,
+            Spectrum::DenselySampled(s) => s.get(lambda),
+            Spectrum::Blackbody(s) => s.get(lambda),
         }
     }
 
     /// Returns a bound on the maximum value of the psectral distribution over its wavelength range.
     /// The primary utility here is computing bounds on the power emitted by light sources so that
     /// lights can be sampled according to their expected contriibution to illumination in the scene.
-    pub fn max_value(&self) -> Float {
+    fn max_value(&self) -> Float {
         match self {
             Spectrum::Constant(c) => c.max_value(),
-            Spectrum::DenselySampled {
-                lambda_min: _,
-                lambda_max: _,
-                values,
-            } => {
-                // PAPERDOC This is a vector of Floats and we must find the minimum.
-                // But what about NaN? In C++, this is a very easy case to miss.
-                // In Rust, the compiler makes you handle NaN or else it won't compile.
-                // e.g. using values.iter().min() will complain that f32 does not satisfy
-                // the Ord constraint (due to NaN). In this case, we opt to fold() instead,
-                // and use f32::min() which itself handles the NaN case. This is a bit more verbose,
-                // but that's a worthwhile trade-off to ensure NaNs are handled correctly at compile-time,
-                // rather tracking down propagated NaNs at runtime.
-
-                // TODO actually fix, maybe need neg_infinity, was using min() before.
-                // Though a list of neg_infinity would technically have its max there...
-                // Maybe we need an option instead.
-                let max = values.iter().fold(Float::NEG_INFINITY, |a, &b| a.max(b));
-                if max == Float::NEG_INFINITY {
-                    panic!("Empty or NaN-filled Densely Sampled Spectrum!")
-                }
-                max
-            }
-            Spectrum::Blackbody {
-                t: _,
-                normalization_factor: _,
-            } => 1.0,
+            Spectrum::DenselySampled(s) => s.max_value(),
+            Spectrum::Blackbody(s) => s.max_value(),
         }
     }
 }
 
-struct Constant {
+pub struct Constant {
     c: Float,
 }
 
@@ -146,6 +62,98 @@ impl SpectrumI for Constant {
 
     fn max_value(&self) -> Float {
         self.c
+    }
+}
+
+pub struct DenselySampled {
+    lambda_min: i32,
+    lambda_max: i32,
+    values: Vec<Float>,
+}
+
+impl DenselySampled {
+    /// Samples from the provided spectrum to create a DenselySampled spectrum
+    pub fn new(spectrum: &Spectrum, lambda_min: i32, lambda_max: i32) -> DenselySampled {
+        // PAPERDOC This is a fun area where idiomatic rust code (map -> collect) is arguably cleaner
+        // than similar C++ code (allowing e.g. const correctness).
+        let values: Vec<Float> = (lambda_min..lambda_max)
+            .map(|lambda: i32| spectrum.get(lambda as Float))
+            .collect();
+        DenselySampled {
+            lambda_min,
+            lambda_max,
+            values,
+        }
+    }
+}
+
+impl SpectrumI for DenselySampled {
+    fn get(&self, lambda: Float) -> Float {
+        let offset = lambda as i32 - self.lambda_min;
+        if offset < 0 || offset > self.values.len() as i32 {
+            return 0.0;
+        }
+        self.values[offset as usize]
+    }
+
+    fn max_value(&self) -> Float {
+        // PAPERDOC This is a vector of Floats and we must find the maximum.
+        // But what about NaN? In C++, this is a very easy case to miss.
+        // In Rust, the compiler makes you handle NaN or else it won't compile.
+        // e.g. using values.iter().max() will complain that f32 does not satisfy
+        // the Ord constraint (due to NaN). We can instead fold() and clearly handle the
+        // NaN case, with a panic!() in this case.
+        let max = self.values.iter().fold(Float::NAN, |a, &b| a.max(b));
+        if max.is_nan() {
+            panic!("Empty or NaN-filled Densely Sampled Spectrum!")
+        }
+        max
+    }
+}
+
+/// Normalized blackbody spectrum where the maximum value at any wavelength is 1.
+pub struct Blackbody {
+    /// Temperature K
+    t: Float,
+    /// Normalization factor s.t. the maximum value is 1.0.
+    normalization_factor: Float,
+}
+
+impl Blackbody {
+    pub fn new(t: Float) -> Blackbody {
+        let lambda_max = 2.8977721e-3 / t; // Wien's displacement law
+        let normalization_factor = 1.0 / Blackbody::blackbody(lambda_max * 1e9, t);
+        Blackbody {
+            t,
+            normalization_factor,
+        }
+    }
+
+    /// The emitted radiance for blackbody at wavelength lambda (nanometers) at temperature (kelvin).
+    fn blackbody(lambda: Float, temperature: Float) -> Float {
+        if temperature < 0.0 {
+            return 0.0;
+        }
+        let c: Float = 299792458.0;
+        let h: Float = 6.62606957e-34;
+        let kb: Float = 1.3806488e-23;
+        // Convert to meters
+        let l = lambda * 1e-9;
+        // TODO consider Exponentiation by squaring for powi call, and fastexp for exp().
+        let le =
+            (2.0 * h * c * c) / l.powi(5) * (Float::exp((h * c) / (l * kb * temperature)) - 1.0);
+        debug_assert!(!le.is_nan());
+        le
+    }
+}
+
+impl SpectrumI for Blackbody {
+    fn get(&self, lambda: Float) -> Float {
+        Blackbody::blackbody(lambda, self.t) * self.normalization_factor
+    }
+
+    fn max_value(&self) -> Float {
+        1.0
     }
 }
 
