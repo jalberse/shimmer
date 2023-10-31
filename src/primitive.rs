4551bd6058c1d4af1a257678d7a16e65453dd650
John Alberse <alberse.john@gmail.com>
2023-10-30 22:09:41 -0500

    Transformed primitive (instancing)

--- src/primitive.rs
+++ src/primitive.rs
@@ -5,6 +5,8 @@ use crate::{
     material::Material,
     ray::Ray,
     shape::{Shape, ShapeI, ShapeIntersection},
+    transform::Transform,
+    vecmath::normal::Normal3,
     Float,
 };
 
@@ -25,28 +27,34 @@ pub trait PrimitiveI {
 
 pub enum Primitive {
     Simple(SimplePrimitive),
+    Transformed(TransformedPrimitive),
 }
 
 impl PrimitiveI for Primitive {
     fn bounds(&self) -> Bounds3f {
         match self {
             Primitive::Simple(p) => p.bounds(),
+            Primitive::Transformed(p) => p.bounds(),
         }
     }
 
     fn intersect(&self, ray: &Ray, t_max: Float) -> Option<ShapeIntersection> {
         match self {
             Primitive::Simple(p) => p.intersect(ray, t_max),
+            Primitive::Transformed(p) => p.intersect(ray, t_max),
         }
     }
 
     fn intersect_predicate(&self, ray: &Ray, t_max: Float) -> bool {
         match self {
             Primitive::Simple(p) => p.intersect_predicate(ray, t_max),
+            Primitive::Transformed(p) => p.intersect_predicate(ray, t_max),
         }
     }
 }
 
+/// A Primitive which simply adds material information to the surface interaction of
+/// the shape.
 pub struct SimplePrimitive {
     shape: Shape,
     material: Rc<Material>,
@@ -65,5 +73,46 @@ impl PrimitiveI for SimplePrimitive {
 
     fn intersect_predicate(&self, ray: &Ray, t_max: Float) -> bool {
         self.shape.intersect_predicate(ray, t_max)
+    }
+}
+
+/// Enables object instancing by wrapping a pointer to a shared primitive with a transform.
+pub struct TransformedPrimitive {
+    primitive: Rc<Primitive>,
+    // TODO We should cache this transform in a pool rather than store a unique one
+    render_from_primitive: Transform,
+}
+
+impl TransformedPrimitive {
+    pub fn new(
+        primitive: &Rc<Primitive>,
+        render_from_primitive: Transform,
+    ) -> TransformedPrimitive {
+        TransformedPrimitive {
+            primitive: primitive.clone(),
+            render_from_primitive,
+        }
+    }
+}
+
+impl PrimitiveI for TransformedPrimitive {
+    fn bounds(&self) -> Bounds3f {
+        self.render_from_primitive.apply(&self.primitive.bounds())
+    }
+
+    fn intersect(&self, ray: &Ray, t_max: Float) -> Option<ShapeIntersection> {
+        let ray = self.render_from_primitive.apply_inv(ray);
+        let mut si = self.primitive.intersect(&ray, t_max)?;
+        debug_assert!(si.t_hit <= 1.001 * t_max);
+
+        si.intr = self.render_from_primitive.apply(&si.intr);
+        debug_assert!(si.intr.interaction.n.dot(&si.intr.shading.n) >= 0.0);
+
+        Some(si)
+    }
+
+    fn intersect_predicate(&self, ray: &Ray, t_max: Float) -> bool {
+        let ray = self.render_from_primitive.apply(ray);
+        self.primitive.intersect_predicate(&ray, t_max)
     }
 }
--- src/transform.rs
+++ src/transform.rs
@@ -508,7 +508,7 @@ impl Transformable for Ray {
 }
 
 impl Transformable for RayDifferential {
-    // TODO note we may also wanta versiont hat calculates the new t_max, or add that to this one
+    // TODO note we may also wanta version that calculates the new t_max, or add that to this one
     fn apply(&self, transform: &Transform) -> Self {
         // Get the transformed base ray
         let tr: Ray = transform.apply(&self.ray);
