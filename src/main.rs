05b7bf372745529b50ee71b5e736260cefa895bc
John Alberse <alberse.john@gmail.com>
2024-01-25 13:28:41 -0600

    Add basic CLI interface with clap
    For now, just passes in the scene file.
    Other named options can be added.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 use std::fs::{self};
 
+use clap::Parser;
 use shimmer::{
     loading::{
         parser,
@@ -14,13 +15,19 @@ use shimmer::{
 };
 use string_interner::StringInterner;
 
+#[derive(clap::Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    scene_file: String,
+}
+
 fn main() {
-    // TODO Parse from command line.
+    let cli = Args::parse();
 
     let mut string_interner = StringInterner::new();
     let mut cached_spectra = std::collections::HashMap::new();
     let mut options = Options::default();
-    let file = fs::read_to_string("scenes/cornell.pbrt").unwrap();
+    let file = fs::read_to_string(cli.scene_file).unwrap();
     let scene = Box::new(BasicScene::default());
     let mut scene_builder = BasicSceneBuilder::new(scene, &mut string_interner);
     parser::parse_str(
