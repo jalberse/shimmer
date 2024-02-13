35316f55acf68fecbd2a6d7ca06052b33f3d5889
John Alberse <alberse.john@gmail.com>
2024-02-13 17:57:00 -0600

    BilinearPatch normal bounds

--- src/shape/bilinear_patch.rs
+++ src/shape/bilinear_patch.rs
@@ -1,8 +1,10 @@
 use std::sync::Arc;
 
 use crate::bounding_box::Bounds3f;
+use crate::direction_cone::DirectionCone;
 use crate::math::lerp;
 use crate::vecmath::point::Point3;
+use crate::vecmath::spherical::cos_theta;
 use crate::vecmath::{Length, Point3f, Vector3f};
 use crate::Float;
 use crate::vecmath::vector::Vector3;
@@ -68,6 +70,13 @@ impl BilinearPatch{
         let p11 = mesh.p[v];
         (p00, p10, p01, p11)
     }
+    fn get_vertex_indices(mesh: &Arc<BilinearPatchMesh>, blp_index: usize) -> (usize, usize, usize, usize)
+    {
+        (mesh.vertex_indices[blp_index * 4],
+        mesh.vertex_indices[blp_index * 4 + 1],
+        mesh.vertex_indices[blp_index * 4 + 2],
+        mesh.vertex_indices[blp_index * 4 + 3])
+    }
 
     fn is_rectangle(mesh: &Arc<BilinearPatchMesh>, blp_index: usize) -> bool 
     {
@@ -114,7 +123,64 @@ impl ShapeI for BilinearPatch
     }
 
     fn normal_bounds(&self) -> crate::direction_cone::DirectionCone {
-        todo!()
+        let (p00, p10, p01, p11) = BilinearPatch::get_points(&self.mesh, self.blp_index);
+        let(v0, v1, v2, v3) = BilinearPatch::get_vertex_indices(&self.mesh, self.blp_index);
+
+        // If the patch is a triangle, return bounds for single surface normal.
+        if p00 == p10 || p10 == p11 || p11 == p01 || p01 == p00
+        {
+            let dpdu = lerp::<Vector3f>(0.5, p10.into(), p11.into()) - lerp::<Vector3f>(0.5, p00.into(), p01.into());
+            let dpdv = lerp::<Vector3f>(0.5, p01.into(), p11.into()) - lerp::<Vector3f>(0.5, p00.into(), p10.into());
+            let n = dpdu.cross(dpdv).normalize();
+            let n = if !self.mesh.n.is_empty()
+            {
+                let n00 = self.mesh.n[v0];
+                let n10 = self.mesh.n[v1];
+                let n01 = self.mesh.n[v2];
+                let n11 = self.mesh.n[v3];
+                let ns = (n00 + n10 + n01 + n11) * 0.25;
+                n.face_forward_n(ns)
+            } else if self.mesh.reverse_orientation ^ self.mesh.transform_swaps_handedness {
+                -n
+            } else {
+                n
+            };
+            return DirectionCone::from_angle(n.into());
+        }
+
+        // Compute bilinear patch normal n00 at (0, 0).
+        let n00 = (p10 - p00).cross(p01 - p00).normalize();
+        let n00 = if !self.mesh.n.is_empty()
+        {
+            n00.face_forward_n(self.mesh.n[v0])
+        } else if self.mesh.reverse_orientation ^ self.mesh.transform_swaps_handedness {
+            -n00
+        } else {
+            n00
+        };
+
+        // Compute bilinear patch normals at n10, n01, n11
+        let n10 = (p11 - p10).cross(p00 - p10).normalize();
+        let n01 = (p00 - p01).cross(p11 - p01).normalize();
+        let n11 = (p01 - p11).cross(p10 - p11).normalize();
+        let (n10, n01, n11) = if !self.mesh.n.is_empty()
+        {
+            (
+                n10.face_forward_n(self.mesh.n[v1]),
+                n01.face_forward_n(self.mesh.n[v2]),
+                n11.face_forward_n(self.mesh.n[v3]),
+            )
+        } else if self.mesh.reverse_orientation ^ self.mesh.transform_swaps_handedness {
+            (-n10, -n01, -n11)
+        } else {
+            (n10, n01, n11)
+        };
+
+        // Compute average normal and return normal bounds for patch;
+        // this is not an exact bounds, but is practically sufficient.
+        let n = (n00 + n10 + n01 + n11).normalize();
+        let cos_theta = [n.dot(n00), n.dot(n10), n.dot(n01), n.dot(n11)].into_iter().min_by(|a, b| a.partial_cmp(b).expect("Unexpected NaN")).unwrap();
+        DirectionCone::new(n.into(), Float::clamp(cos_theta, -1.0, 1.0))
     }
 
     fn intersect(&self, ray: &crate::ray::Ray, t_max: Float) -> Option<super::ShapeIntersection> {
